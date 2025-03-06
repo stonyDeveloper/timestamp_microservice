00ddf47363a9e6a573b68874da66e8ab44f54a82
stonyDeveloper <stoneachi@gmail.com>
2025-03-06 17:58:58 +0100

    Prepare for Vercel deployment

--- index.js
+++ index.js
@@ -54,6 +54,4 @@ app.get("/api/:date?", function (req, res) {
 });
 
 // Listen on port set in environment variable or default to 3000
-var listener = app.listen(process.env.PORT || 3000, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
-});
+module.exports = app;
